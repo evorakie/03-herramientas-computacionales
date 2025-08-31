7cb8294c347965d259b18127fd115707064547be
Sayen Padilla <antonia.padilla.o@ug.uchile.cl>
2025-08-31 17:55:04 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var valor = document.getElementById("pecinco").offsetWidth;
+
+window.addEventListener("resize", function () {
+    valor = document.getElementById("pecinco").offsetWidth;
+});
+
+function setup() {
+    createCanvas(valor, valor, WEBGL).parent("#pecinco");
+    select("#nombre").html("marti U・ᴥ・U");
+    select("#aprendizaje").html("a programar y explorar nuevas formas de expresión visual para proyectos digitales");
+    noStroke();
+}
+function draw() {
+    background(216, 191, 216);
+    orbitControl();
+    directionalLight(200, 200, 200, -1, -1, -1);
+    pointLight(255, 255, 255, 200, -150, 200);
+    specularMaterial(200, 200, 255);
+    shininess(50);
+    emissiveMaterial(50, 50, 100);
+    metalness(1);
+    fill(139, 7, 139);
+    sphere(40, 64, 64);
+}
+
+function windowResized() {
+    resizeCanvas(valor, valor);
+}
