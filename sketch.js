5e60fa026e252b37183f90552b7a3bfca7cbbb78
Sayen Padilla <antonia.padilla.o@ug.uchile.cl>
2025-08-31 23:21:09 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,17 +15,18 @@ function draw() {
     orbitControl();
     directionalLight(200, 200, 200, -1, -1, -1);
     pointLight(255, 255, 255, 200, -150, 200);
-    specularMaterial(199,21,133);
+    specularMaterial(219, 50, 135);
     shininess(50);
     emissiveMaterial(50, 50, 100);
     metalness(1);
-    fill(199,21,133);
+    fill(219, 50, 135);
     sphere(40, 64, 64);
 }
 
 function windowResized() {
     resizeCanvas(valor, valor);
 }
+
 
 
 
